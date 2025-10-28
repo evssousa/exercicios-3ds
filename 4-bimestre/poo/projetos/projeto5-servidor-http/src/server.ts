ad662ed3e6632ab5eb3f51df97bdeb278cf2d1e3
Rosiele Sousa <rosielesousa49@gmail.com>
2025-10-28 14:39:16 +0000

    Projeto Concluído!

---
+++ 4-bimestre/poo/projetos/projeto5-servidor-http/src/server.ts
@@ -0,0 +1,19 @@
+import { createServer } from "node:http"
+
+let nome: string = "Jonh"
+let idade: number = 34
+let token: string = process.env.PORT as string
+
+const server = createServer((req, res) => {
+    res.end(`Servidor rodando para ${nome}, idade ${idade}!`)
+})
+
+server.listen(3001, () => {
+    console.log(`Servidor funcionando em http://localhost:${token}`)
+})
+
+server.close(() => {
+  console.log("Servidor encerrado.")
+})
+
+export default server
