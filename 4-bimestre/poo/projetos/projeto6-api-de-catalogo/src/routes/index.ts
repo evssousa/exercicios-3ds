be700bc8b0d94acefb431ae0d7c50e5148af56a5
Antonio-Alisson-Teixeira-Cruz <ravenjouki@gmail.com>
2025-11-12 11:33:08 +0000

    up

--- 4-bimestre/poo/projetos/projeto6-api-de-catalogo/src/routes/index.ts
+++ 4-bimestre/poo/projetos/projeto6-api-de-catalogo/src/routes/index.ts
@@ -1,15 +1,16 @@
 import express from "express"
 import produtoRouter from "./produto"
-import pingRouter from "./ping"
 
 const router = express.Router()
 
-router.use("/ping", pingRouter)
 router.use("/produtos", produtoRouter)
 router.get("/", (req, res) => {
     let nome: string = "Rav"
     let idade: number = 16
     res.json({ nome: nome ,idade: idade})
+})
+router.get("/ping", (req, res) => {
+    res.json({ pong: true })
 })
 
 export default router
--- 4-bimestre/poo/projetos/projeto6-api-de-catalogo/src/routes/ping.ts
+++
@@ -1,9 +0,0 @@
-import express from "express"
-
-const router = express.Router()
-
-router.get("/", (req, res) => {
-    res.json({ pong: true })
-})
-
-export default router
