e59700aedc983bb5587aca5aceedc1b01f08fe4a
Rosiele Sousa <rosielesousa49@gmail.com>
2025-11-12 11:34:37 +0000

    correção no projeto e conclusão

--- 4-bimestre/poo/projetos/projeto6-api-de-catalogo/src/routes/index.ts
+++ 4-bimestre/poo/projetos/projeto6-api-de-catalogo/src/routes/index.ts
@@ -3,11 +3,13 @@ import produtosRouter from "./produtos"
 
 const router = express.Router()
 
-
 router.get("/", (req, res) => {
   let nome: string = "Everson"
   let idade: number = 34
   res.json({ nome, idade })
+})
+router.get("/ping", (req, res) => {
+  res.json({ pong: true })
 })
 
 router.use("/produtos", produtosRouter)
--- 4-bimestre/poo/projetos/projeto6-api-de-catalogo/src/routes/produtos.ts
+++ 4-bimestre/poo/projetos/projeto6-api-de-catalogo/src/routes/produtos.ts
@@ -1,6 +1,6 @@
-import { Router, Request, Response } from "express"
+import Express from "express"
 
-const router = Router();
+const router = Express.Router();
 
 let produtos = [
   { id: 1, nome: "Notebook", preco: 3500 },
@@ -8,36 +8,17 @@ let produtos = [
   { id: 3, nome: "Teclado Mecânico", preco: 400 },
 ]
 
-router.get("/", (req: Request, res: Response) => {
+router.get("/", (req, res) => {
   res.json(produtos)
 })
 
-router.get("/:id", (req: Request, res: Response) => {
+router.get("/:id", (req, res) => {
   const id = Number(req.params.id)
   const produto = produtos.find((p) => p.id === id)
 
-  if (!produto) {
-    return res.status(404).json({ erro: "Produto não encontrado" })
-  }
-
   res.json(produto)
 })
-
-router.post("/", (req: Request, res: Response) => {
-  const { nome, preco } = req.body
-
-  if (!nome || !preco) {
-    return res.status(400).json({ erro: "Nome e preço são obrigatórios" })
-  }
-
-  const novoProduto = {
-    id: produtos.length + 1,
-    nome,
-    preco,
-  }
-
-  produtos.push(novoProduto)
-  res.status(201).json(novoProduto)
+router.post("/", (req,res)=>{
+  res.json({"id": 99, "nome": "Webcam", "preco": 300 })
 })
-
 export default router
